2242096af320f3157a4c4e13fc16b23d7cfc9f05
王波 <wangbo@vikadata.com>
2021-10-15 18:45:12 +0800

    feat: initialize the project

---
+++ README.md
@@ -0,0 +1,3 @@
+# 开发者模板小组件
+
+一个`javascript`的`hello world`小组件模板
---
+++ src/setting.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSettingsButton } from '@vika/widget-sdk';
+
+export const Setting = () => {
+  const [isSettingOpened] = useSettingsButton();
+
+  return isSettingOpened ? (
+    <div style={{ flexShrink: 0, width: '300px', borderLeft: 'solid 1px gainsboro', paddingLeft: '16px' }}>
+      this is settings <br />
+      点击右上角的齿轮按钮，或者左侧 setting 属性的 toggle 按钮可以看到我的变化
+    </div>
+  ) : null;
+};
